52b20cb1ad10d77c74e6a9bc0b697317f2609f7b
Samu Viitanen <ss-peliprojekti@gmail.com>
2014-10-16 12:37:17 +0300

    Added level 2

---
+++ ProjectSettings/Assets/Scripts/ContinueButton.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ContinueButton : MonoBehaviour {
+
+	public Color defaultColor;
+	public Color selectedColor;
+	private Material mat;
+	
+	void Start(){
+		mat = renderer.material;
+		mat.color = defaultColor;
+	}
+	
+	void OnTouchDown(){
+		mat.color = selectedColor;
+		Debug.Log ("User clicked " + this.name);
+	}
+	void OnTouchUp(){
+		mat.color = defaultColor;
+		Debug.Log ("Changing map");
+		Application.LoadLevel ("Tut_Level2");
+	}
+	void OnTouchStay(){
+		mat.color = selectedColor;
+	}
+	void OnTouchExit(){
+		mat.color = defaultColor;
+	}
+}
---
+++ ProjectSettings/Assets/Scripts/Touchable.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Touchable : MonoBehaviour {
+
+	public Color defaultColor;
+	public Color selectedColor;
+	private Material mat;
+	
+	void Start(){
+		mat = renderer.material;
+		mat.color = defaultColor;
+	}
+	
+	void OnTouchDown(){
+		mat.color = selectedColor;
+		Debug.Log ("User touched " + this.name);
+		Debug.Log ("Make something happen");
+	}
+	void OnTouchUp(){
+		mat.color = defaultColor;
+	}
+	void OnTouchStay(){
+		mat.color = selectedColor;
+	}
+	void OnTouchExit(){
+		mat.color = defaultColor;
+	}
+}
