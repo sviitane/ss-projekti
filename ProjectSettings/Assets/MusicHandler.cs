0c50788c6f4a2e87d4560f1a4a35d546fa0f7629
Sandra Matilainen <sandramatilainen1@hotmail.com>
2014-10-21 16:02:50 +0300

    MusicScripts
    MusicManager and handler

---
+++ ProjectSettings/Assets/MusicHandler.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicHandler : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+		if (Application.loadedLevelName.Equals("Tut_Level1")) {
+
+						SendMessage ("PlayMusic", "Today");
+				}
+	
+	}
+		
+	// Update is called once per frame
+	void Update () {
+	
+	//	if (Input.GetKeyDown (KeyCode.Q)) {
+	//			
+	//		SendMessage("PlayMusic","Today");
+	//
+	//	}
+
+	}
+
+}
---
+++ ProjectSettings/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioManager : MonoBehaviour {
+
+	public string [] audioNames;
+	public AudioClip [] audioClips;
+	public bool clipFound;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void PlayMusic(string clipName){
+
+			for (int i = 0; i < audioNames.Length; i++) {
+		
+				if(clipName == audioNames[i]) {
+					
+					gameObject.audio.clip = audioClips[i];
+					gameObject.audio.Play();
+					clipFound = true;
+					DontDestroyOnLoad(this.gameObject);
+				break;
+				}
+					else {
+						clipFound = false;
+					}
+			}
+
+	if (!clipFound) 
+		Debug.Log ("Audioclip not found");
+
+
+	}
+}
