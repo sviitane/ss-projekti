7b6c1e5f3a42b6ea040ca442b16963772f27efab
Sandra Matilainen <sandramatilainen1@hotmail.com>
2014-11-06 11:49:03 +0200

    Save
    Save feature, singleton

---
+++ ProjectSettings/Assets/Scripts/AlterGameControls.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AlterGameControls : MonoBehaviour {
+
+	public void ChageExperience(int exp){
+
+		GameControl.control.experience = GameControl.control.experience + exp;
+	}
+
+	public void ChageHealth(int helath){
+		
+		GameControl.control.health = GameControl.control.health + helath;
+	}
+
+	public void ChageKarma(int karma){
+		
+		GameControl.control.karma = GameControl.control.karma + karma;
+	}
+
+	public void ChageChaos(int chaos){
+		
+		GameControl.control.chaos = GameControl.control.chaos + chaos;
+			
+	}
+}
---
+++ ProjectSettings/Assets/Scripts/GameControl.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+public class GameControl : MonoBehaviour {
+
+	public static GameControl control;
+
+	public int health;
+	public int experience;
+	public int karma;
+	public int chaos;
+
+	public void Awake () {
+	
+		if (control != null) {
+
+			DontDestroyOnLoad(gameObject);
+			control = this;
+		}
+		if (control != this) {
+				
+			Destroy(gameObject);
+		}
+
+	}
+
+	public void OnGUI(){
+//
+//		GUI.Label( Rect (10, 10, 100, 30), "Health: " );
+//		GUI.Label( Rect (10, 10, 120, 30), "Exp : " );
+	}
+
+
+	public void Save(){
+		BinaryFormatter bf = new BinaryFormatter ();
+		FileStream file = File.Open (Application.persistentDataPath +"/playerInfo.dat", FileMode.Open);
+
+	}
+
+}
