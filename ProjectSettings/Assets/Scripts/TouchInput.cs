84a778d5dd1b214b78c2a8ad4dfa278956e6443b
Samu Viitanen <ss-peliprojekti@gmail.com>
2014-11-06 11:35:24 +0200

    Guiloader
    Changed GUI to Guiloader script

--- ProjectSettings/Assets/Scripts/TouchInput.cs
+++ ProjectSettings/Assets/Scripts/TouchInput.cs
@@ -48,12 +48,13 @@ public class TouchInput : MonoBehaviour {
 					recipient.SendMessage("OnTouchStay", hit.point, SendMessageOptions.DontRequireReceiver);
 				}
 			}
-			
+			/*
 			foreach(GameObject g in touchesOld){
 				if(!touchList.Contains(g)){
 					g.SendMessage("OnTouchExit", hit.point, SendMessageOptions.DontRequireReceiver);
 				}
 			}
+			*/
 		}
 #endif
 
