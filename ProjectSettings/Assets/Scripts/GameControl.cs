8613bf418916fa2e35d60b4d9011208f1fdbef28
Sandra Matilainen <sandramatilainen1@hotmail.com>
2014-11-06 12:59:20 +0200

    save
    serialization, save to filepath and load from it

--- ProjectSettings/Assets/Scripts/GameControl.cs
+++ ProjectSettings/Assets/Scripts/GameControl.cs
@@ -27,16 +27,55 @@ public class GameControl : MonoBehaviour {
 
 	}
 
-	public void OnGUI(){
-//
-//		GUI.Label( Rect (10, 10, 100, 30), "Health: " );
-//		GUI.Label( Rect (10, 10, 120, 30), "Exp : " );
+	public void ShowBasicPlayerInfo(){
+
+//		GUI.Label(new Rect (10, 10, 100, 30), "Health: " + health);
+//		GUI.Label(new Rect (10, 10, 120, 30), "Exp : " + experience);
 	}
 
 
 	public void Save(){
-		BinaryFormatter bf = new BinaryFormatter ();
-		FileStream file = File.Open (Application.persistentDataPath +"/playerInfo.dat", FileMode.Open);
+		BinaryFormatter bf = new BinaryFormatter();
+		FileStream file = File.Create(Application.persistentDataPath +"/playerInfo.dat");
+
+		PlayerData data = new PlayerData();
+		data.health = health;
+		data.experience = experience;
+		data.chaos = chaos;
+		data.karma = karma;
+
+		bf.Serialize (file, data);
+
+		file.Close();
+	}
+
+	public void Load(){
+
+		if (File.Exists (Application.persistentDataPath + "/playerInfo.dat")) {
+						BinaryFormatter bf = new BinaryFormatter ();
+						var appPath = Application.persistentDataPath + "/playerInfo.dat";
+						FileStream file = File.Open(appPath, FileMode.Open);
+
+						PlayerData data = (PlayerData)bf.Deserialize (file);
+						health = data.health;	
+						experience = data.experience;
+						chaos = data.chaos;
+						karma= data.karma;
+		} else {
+			health = 100;	
+			experience = 0;
+			chaos = 0;
+			karma= 0;
+		}
+	}
+	
+	[Serializable]
+	class PlayerData {
+		
+		public int health;
+		public int experience;
+		public int karma;
+		public int chaos;
 
 	}
 
