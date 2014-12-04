ff6962568dc0f86cc01a5c9adaae9f61ddc2276e
Sandra Matilainen <sandramatilainen1@hotmail.com>
2014-12-04 17:32:56 +0200

    Load text
    and few images

---
+++ ProjectSettings/Assets/Scripts/TextHandler.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Text;
+using System.IO;
+
+public class TextHandler : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	private bool LoadText(string fileName)
+	{
+		// Handle any problems that might arise when reading the text
+		try
+		{
+			string line;
+			// Create a new StreamReader, tell it which file to read and what encoding the file
+			// was saved as
+			StreamReader theReader = new StreamReader(fileName, Encoding.Default);
+			
+			// Immediately clean up the reader after this block of code is done.
+			// You generally use the "using" statement for potentially memory-intensive objects
+			// instead of relying on garbage collection.
+			// (Do not confuse this with the using directive for namespace at the
+			// beginning of a class!)
+			using (theReader)
+			{
+				// While there's lines left in the text file, do this:
+				do
+				{
+					line = theReader.ReadLine();
+					if (line != null)
+					{
+						// Do whatever you need to do with the text line, it's a string now
+						// In this example, I split it into arguments based on comma
+						// deliniators, then send that array to DoStuff()
+						string[] entries = line.Split(',');
+//						if (entries.Length > 0)
+//							DoStuff(entries);
+					}
+				}
+				while (line != null);
+				
+				// Done reading, close the reader and return true to broadcast success
+				theReader.Close();
+				return true;
+			}
+		}
+		
+		// If anything broke in the try block, we throw an exception with information
+		// on what didn't work
+		catch (System.Exception e)
+		{
+			Debug.Log ("Trouble loading text" + e.Message);
+
+			return false;
+		}
+	}
+}
