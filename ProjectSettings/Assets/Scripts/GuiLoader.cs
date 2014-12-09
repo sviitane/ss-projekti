4be50f82e0573c9d536963231bffd3927c221607
Samu Viitanen <ss-peliprojekti@gmail.com>
2014-12-09 14:43:13 +0200

    Added storymode in guiloader and some text in it
    Storymode is used in guiloader, story is written in segments of text.

--- ProjectSettings/Assets/Scripts/GuiLoader.cs
+++ ProjectSettings/Assets/Scripts/GuiLoader.cs
@@ -1,14 +1,23 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+[System.Serializable]
+public class StoryText{
+	public string text;
+	public bool canChoose;
+	public string goodOption;
+	public string badOption;
+	public string next;
+}
+
 public class GuiLoader : MonoBehaviour {
 
 	public bool storyMode;
 
 	// All level story texts
-	public string[] levelOneStoryTexts;
-	public string[] levelTwoStoryTexts;
-	public string[] levelThreeStoryTexts;
+	public StoryText[] levelOneStory;
+	public StoryText[] levelTwoStory;
+	public StoryText[] levelThreeStory;
 
 	public Texture2D checkAction;
 	public Texture2D continueAction;
@@ -37,10 +46,15 @@ public class GuiLoader : MonoBehaviour {
 	private float boxHeight = 240;
 	private float boxWidth = 50;
 
+	private float bigBoxHeight;
+
+	private float currentStorySlide;
+
 	void Start(){
 		mapCleared = false;
 		mapClearText = "Congratulations, you have completed all actions in" + Application.loadedLevelName + ", click on the arrow to continue";
 		nextLevel = Application.loadedLevel + 1;
+		currentStorySlide = 0;
 	}
 
 	void Awake(){
@@ -65,6 +79,7 @@ public class GuiLoader : MonoBehaviour {
 
 		boxWidth = Screen.width;
 		boxHeight = Screen.height / 3;
+		bigBoxHeight = Screen.height;
 
 		// Only show textbox if there is some text to show, don't show any other text boxes if map has been cleared
 		if(!storyMode){
@@ -98,11 +113,18 @@ public class GuiLoader : MonoBehaviour {
 			}
         }else if(storyMode){
 			Debug.Log("Story mode active");
-			if(Application.loadedLevel.Equals(1) && (levelOneStoryTexts.GetLength(0)>0)){
-				
-			}else if(Application.loadedLevel.Equals(2) && (levelTwoStoryTexts.GetLength(0)>0)){
+			if(Application.loadedLevel.Equals(1) && (levelOneStory.GetLength(0)>0)){
+				if(currentStorySlide < levelOneStory.GetLength(0)){
+					StoryText t = (StoryText)levelOneStory.GetValue((long)currentStorySlide);
+					setupBigBox(t);
+				}else{
+					Debug.Log("Story for this map is finished");
+					text = "";
+					storyMode = false;
+				}
+			}else if(Application.loadedLevel.Equals(2) && (levelTwoStory.GetLength(0)>0)){
 
-			}else if(Application.loadedLevel.Equals(3) && (levelThreeStoryTexts.GetLength(0)>0)){
+			}else if(Application.loadedLevel.Equals(3) && (levelThreeStory.GetLength(0)>0)){
 
 			}else{
 				Debug.LogWarning("Current level has no story text specified, setting story mode to false");
@@ -114,5 +136,38 @@ public class GuiLoader : MonoBehaviour {
 	public void changeText(string newtext){
 		Debug.Log ("Text changed");
 		this.text = newtext;
+	}
+
+	public void setupBigBox(StoryText t){
+		text = t.text;
+		GUI.skin.font = f;
+		GUI.backgroundColor = Color.white;
+		GUI.Box (new Rect (0, 0, boxWidth, bigBoxHeight), "Story", customBoxStyle);
+		GUI.Label (new Rect (30, 40, boxWidth - 60, bigBoxHeight - 50), text, customLabelStyle);
+		if(t.canChoose){
+			if(!t.goodOption.Equals(null) && !t.badOption.Equals(null)){
+				if(GUI.Button (new Rect (30, bigBoxHeight - 150, boxWidth - 60, bigBoxHeight - 50), t.goodOption, customLabelStyle)){
+					Debug.Log("Good option clicked");
+					InteractiveAudioManager.audioManager.PlaySound("blop");
+					currentStorySlide = currentStorySlide + 1;
+				}
+				if(GUI.Button (new Rect (30, bigBoxHeight - 100, boxWidth - 60, bigBoxHeight - 50), t.badOption, customLabelStyle)){
+					InteractiveAudioManager.audioManager.PlaySound("blop");
+					currentStorySlide = currentStorySlide + 1;
+				}
+			}else{
+				Debug.LogError("If options are enabled, you must specify the option texts!");
+			}
+		}else{
+			if(!t.next.Equals(null)){
+				if(GUI.Button (new Rect (30, bigBoxHeight - 150, boxWidth - 60, bigBoxHeight - 50), t.next, customLabelStyle)){
+					Debug.Log("Next clicked");
+					InteractiveAudioManager.audioManager.PlaySound("blop");
+					currentStorySlide = currentStorySlide + 1;
+				}
+			}else{
+				Debug.LogError("You must specify a text for continuing");
+			}
+		}
 	}
 }
